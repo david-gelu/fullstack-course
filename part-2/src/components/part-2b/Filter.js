165dafb4012ae566df13dc4f6936b8c903d0196b
david-gelu <david.gelu90@gmail.com>
2021-12-02 17:03:39 +0200

    feat: use data fron server

--- part-2/src/components/part-2b/Filter.js
+++ part-2/src/components/part-2b/Filter.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-const Filter = ({ personsArr, setPersons }) => {
+const Filter = ({ persons, setPersons }) => {
   const [search, setSearch] = useState('')
 
   useEffect(() => {
-    setPersons(personsArr.filter(person => person.name.toLowerCase().includes(search.toLowerCase())))
+    setPersons(persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search])
 
--- part-2/src/components/part-2b/PhoneBook.js
+++ part-2/src/components/part-2b/PhoneBook.js
@@ -1,26 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import axios from 'axios'
+
 
 const PhoneBook = () => {
-  const personsArr = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
-  const [persons, setPersons] = useState(personsArr)
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
         <Filter
-          personsArr={personsArr}
+          persons={persons}
           setPersons={setPersons} />
         <br />
         <h3> Add new contact</h3>
