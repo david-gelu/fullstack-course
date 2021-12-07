2d44cf416ee51bce69b6d7ada23b3551306c6ef4
david-gelu <david.gelu90@gmail.com>
2021-12-07 15:43:58 +0200

    refactor: remove unused prop and remove whitespace

--- part-2/src/components/part-2c/Filter.js
+++ part-2/src/components/part-2c/Filter.js
@@ -1,23 +1,18 @@
 import React, { useEffect } from 'react'
 
-const Filter = ({ countries, setCountries, search, setSearch, setSearchData }) => {
+const Filter = ({ countries, search, setSearch, setSearchData }) => {
 
   useEffect(() => {
     setSearchData(countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase())))
-
   }, [countries, search, setSearchData])
 
-  const searchName = (e) => {
-    setSearch(e.target.value ?? null)
+  const searchName = (e) => { setSearch(e.target.value ?? null) }
 
-
-  }
   return (
     <form>
       find countries <input type='search' value={search} onChange={searchName} />
     </form>
   )
-
 }
 
 export default Filter
